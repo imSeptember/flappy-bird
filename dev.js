438a46c7ac90178d1b16b908407d1c297a44e139
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-10-01 18:18:33 +0300

    321

--- dev.js
+++ dev.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const textArr = [
+    "Press 'W' to jump!",
+    " Dodge pipes!",
+    " Successfully avoid 10 pipes to win the game!",
+    "Made by V. Kalenskiy",
+    "for RSSchool",
+    "2023",
+  ];
+
   const settings = document.querySelector(".settings");
   const console = document.querySelector(".console-container");
 
@@ -6,9 +15,11 @@ document.addEventListener("DOMContentLoaded", () => {
     if (settings.classList.contains("hide")) {
       console.style.display = "block";
       settings.classList.toggle("hide");
+      settings.style.animation = "rotateAnimation 4s linear infinite";
     } else {
       console.style.display = "none";
       settings.classList.toggle("hide");
+      settings.style.animation = "";
     }
   }
 
@@ -20,13 +31,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const text = document.querySelector(".changedText");
 
   function changeDevops() {
-    text.textContent = "1231";
+    text.textContent = `
+    ${textArr[3]}
+    ${textArr[4]}
+    ${textArr[5]}`;
   }
 
   function changeRules() {
-    text.textContent = "Press 'W' to jump!";
+    text.textContent = `
+    ${textArr[0]}
+    ${textArr[1]}
+    ${textArr[2]}`;
   }
-
   devopsBnt.addEventListener("click", changeDevops);
   rulesBnt.addEventListener("click", changeRules);
 });
